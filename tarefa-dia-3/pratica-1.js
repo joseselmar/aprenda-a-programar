0c80a0ab7a7973a3ccb3fb7cfd359f56a366c674
José Selmar Junior <joseselmarjunior@JoseSelmar.local>
2022-05-18 20:09:29 -0400

    atualizacao da atividade1 da aula3

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -4,3 +4,5 @@ let profissao = 'analista de sistemas';
 let email = 'selmar.unimedbv@gmail.com';
 let proximas_ferias = 'machu picchu';
 
+console.log('O nome do aluno é: ',nome,'-',profissao,',',idade,'anos', '- email:',email,
+'e sonha em conhecer:',proximas_ferias);
